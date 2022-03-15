94e4089826eefa59ab651fdd8859d914b985643d
overtoo <s_bajo@hotmail.com>
2022-03-15 17:39:03 +0800

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
+module.exports = {
+  env: {
+    MONGO_URI:
+      "mongodb+srv://userbob:aqzaqz12@cluster0.x1li3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  },
+};
+  
